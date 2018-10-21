34304cacc9c008d9e4cc8e60031e0039a42d54e6
蒋益民 <jiangyimin606@outlook.com>
2018-10-21 23:24:29 +0800

    20181021-2

---
+++ QTS.Strategy/MainViewModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+using System.Collections.Generic;
+using QTS.Strategy.Models;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace QTS.Strategy
+{
+    [Export(typeof(MainViewModel))]
+    public class MainViewModel : Conductor<IScreen>.Collection.OneActive, IConductor, IHandle<ModelEvents>
+    {
+        IEnumerable<IScreen> myScreens;
+        private readonly IEventAggregator _events;
+        [ImportingConstructor]
+        public MainViewModel([ImportMany]IEnumerable<IScreen> screens, IEventAggregator events)
+        {
+            this.myScreens = screens;
+            Items.Clear();
+            this._events = events;
+            _events.Subscribe(this);
+            DisplayName = "Quant Book Project";
+        }
+
+        private string chapter;
+        public string Chapter
+        {
+            get { return chapter; }
+            set
+            {
+                chapter = value;
+                NotifyOfPropertyChange(() => Chapter);
+            }
+        }
+
+        public void Handle(ModelEvents evnt)
+        {
+            List<object> lst = evnt.EventList;
+            StatusText = lst[0].ToString();
+            if (lst.Count > 1)
+            {
+                ProgressMin = Convert.ToInt32(lst[1]);
+                ProgressMax = Convert.ToInt32(lst[2]);
+                ProgressValue = Convert.ToInt32(lst[3]);
+            }
+        }
+
+        private int progressMin = 0;
+        public int ProgressMin
+        {
+            get { return progressMin; }
+            set
+            {
+                progressMin = value;
+                NotifyOfPropertyChange(() => ProgressMin);
+            }
+        }
+
+        private int progressMax = 1;
+        public int ProgressMax
+        {
+            get { return progressMax; }
+            set
+            {
+                progressMax = value;
+                NotifyOfPropertyChange(() => ProgressMax);
+            }
+        }
+
+        private int progressValue = 0;
+        public int ProgressValue
+        {
+            get { return progressValue; }
+            set
+            {
+                progressValue = value;
+                NotifyOfPropertyChange(() => ProgressValue);
+            }
+        }
+        private string statusText;
+        public string StatusText
+        {
+            get { return statusText; }
+            set
+            {
+                statusText = value;
+                NotifyOfPropertyChange(() => StatusText);
+            }
+        }
+        
+
+       
+        public void OnClick(object sender)
+        {
+            Button btn = sender as Button;
+            int num = Convert.ToInt16(btn.Content.ToString().Split(' ')[1]);
+            string ch = "Ch";
+            if (num < 10)
+                ch += "0" + num.ToString();
+            else
+                ch += num.ToString();
+           
+            Items.Clear();
+            var sc = from q in myScreens where q.ToString().Contains(ch) orderby q.DisplayName select q;
+            Items.AddRange(sc);
+
+            var view = this.GetView() as MainView;
+            foreach (Button b in view.buttonPanel.Children)
+            {
+                b.Background = Brushes.Transparent;
+                b.Foreground = Brushes.Black;
+            }
+
+            btn.Background = Brushes.Black;
+            btn.Foreground = Brushes.White;
+        }
+    }
+}
---
+++ QTS.Strategy/Models/ModelEvents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QTS.Strategy.Models
+{
+    public class ModelEvents
+    {
+        public List<object> EventList { get; private set; }
+        public ModelEvents(List<object> objects)
+        {
+            this.EventList = objects;
+        }
+    }
+}
