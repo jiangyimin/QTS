e3ce232905209675c0ff3642f9b0973fa0a7a7a3
jiangyimin <jiangyimin@126.com>
2018-11-06 07:58:16 +0800

    20181106-1

---
+++ QTS.Strategy/Ch05/MySimpleLineViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Caliburn.Micro;
+using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Media;
+using ChartControl;
+
+namespace QTS.Strategy.Ch05
+{
+    [Export(typeof(IScreen)), PartCreationPolicy(CreationPolicy.NonShared)]
+    public class MySimpleLineViewModel : Screen
+    {
+        [ImportingConstructor]
+        public MySimpleLineViewModel()
+        {
+            DisplayName = "05. My Simple Line";            
+            DataCollection = new BindableCollection<LineSeries>();            
+        }
+
+        public BindableCollection<LineSeries> DataCollection { get; set; }
+
+        public void AddChart()
+        {
+            DataCollection.Clear();
+            LineSeries ls = new LineSeries();
+            ls.LineColor = Brushes.Blue;
+            ls.LineThickness = 2;
+            ls.LinePattern = LinePatternEnum.Solid;
+            for (int i = 0; i < 50; i++)
+            {
+                double x = i / 5.0;
+                double y = Math.Sin(x);
+                ls.LinePoints.Add(new Point(x, y));
+            }
+            DataCollection.Add(ls);
+        }
+    }
+}
